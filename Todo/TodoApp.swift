cb6697878048efa6599fd4f93dfa893e09cc65f8
Enes Kızılcin <enes.kizilcin@gmail.com>
2022-04-14 14:06:17 +0300

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Nazif Enes Kızılcin on 14.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
